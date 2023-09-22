1e96053ab513c97be0132425ab16a205ce64049a
Marcelo Morgado <01101101.mgd@gmail.com>
2023-09-22 11:29:16 -0300

    trilha-net-oop-desafio added

---
+++ trilha-net-oop-desafio/Models/Smartphone.cs
@@ -0,0 +1,34 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+        
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
+
+
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ trilha-net-oop-desafio/Program.cs
@@ -0,0 +1,27 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("(00)1111-1111", "Iphone12", "IMEI1232123", 6);
+Nokia nokia = new Nokia("(00)1111-2222", "Nokia 5", "IMEI4234234552", 4);
+List<Smartphone> smartphones = new List<Smartphone>();
+smartphones.Add(iphone);
+smartphones.Add(nokia);
+
+try 
+{
+    foreach (Smartphone smartphone in smartphones)
+    {
+        Console.WriteLine("\nTESTE DO SMARTPHONE: {0}.", smartphone.Modelo);
+        smartphone.InstalarAplicativo("TestPhone");
+        Console.WriteLine($"Numero: {smartphone.Numero}");
+        smartphone.Ligar();
+        smartphone.ReceberLigacao();
+        Console.WriteLine($"IMEI: {smartphone.IMEI}\nMemória Ram: {smartphone.Memoria}");
+        Console.WriteLine("Teste do smartphone {0} foi concluído com sucesso.\n", smartphone.Modelo);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Falha ao executar o Teste. ERRO: {ex.Message}");
+}
